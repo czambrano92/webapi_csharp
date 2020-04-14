faa61baba2380e0f976da07bebe33906df674d40
cesar <cesar@DESKTOP-FCUITAJ>
2020-04-14 15:48:44 -0400

    First commit using VS 2019 and Github First simple API using .NET Core C#

---
+++ Modelos/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_csharp.Modelos
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public  String Nombre { get; set; }
+        public String Apellido { get; set; }
+    }
+}
